05438273f1cc231626aebee53ffcd68b6136f1a4
MuhRehman <paki_jan@yahoo.com>
2020-09-06 23:53:48 +0500

    Updates

---
+++ precache-manifest.7efacab488266111dfcd61f1644bdff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcb9d261b8623cc1c197415cddfca175",
+    "url": "/e-shopping-page-/index.html"
+  },
+  {
+    "revision": "1d6756be3d8b877fe0a2",
+    "url": "/e-shopping-page-/static/css/main.8f6d1047.chunk.css"
+  },
+  {
+    "revision": "60adf38556c9ac64232a",
+    "url": "/e-shopping-page-/static/js/2.b2eadc8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/e-shopping-page-/static/js/2.b2eadc8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d6756be3d8b877fe0a2",
+    "url": "/e-shopping-page-/static/js/main.60a4983c.chunk.js"
+  },
+  {
+    "revision": "4a2e0d041c08bcd5db62",
+    "url": "/e-shopping-page-/static/js/runtime-main.7707919d.js"
+  }
+]);
